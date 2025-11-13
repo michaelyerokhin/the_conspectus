92a970e635000975adeb8077ef9aeca980cdb1de
Long Nguyen <nguyenv@brandeis.edu>
2025-11-12 19:37:40 -0500

    doc(frontend/types): explained CurrentUser

--- apps/frontend/app/lib/types.ts
+++ apps/frontend/app/lib/types.ts
@@ -2,6 +2,10 @@
  * Shared types that can be used in both server and client components
  */
 
+/** 
+ * Note that this user is different from the Supabase user (has email attribute)
+ * This user type is returned by supabaseAdmin.auth.admin.getUserById
+ */
 export type CurrentUser = {
   id: string;
   email: string;
